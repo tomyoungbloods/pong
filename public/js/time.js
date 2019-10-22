10931d46b788d05629631495dfb131d11342140a
tomjongbloets <tomjongbloets@live.nl>
2019-10-22 14:47:35 +0200

    Klok start werkt nu

--- public/js/time.js
+++ public/js/time.js
@@ -11,3 +11,16 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+function startKnockOut(h, m) {
+  var now = new Date();
+  var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), h, m, 0, 0) - now;
+  if (millisTill10 < 0) {
+      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+  }
+  setTimeout(function(){ window.location.replace('/session-builder') }, millisTill10);
+}
+
+startKnockOut(10, 38);
+startKnockOut(13, 08);
+startKnockOut(15, 08);
--- resources/js/time.js
+++ resources/js/time.js
@@ -11,3 +11,10 @@ function startTime() {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
   }
+
+  var now = new Date();
+  var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 34, 0, 0) - now;
+  if (millisTill10 < 0) {
+       millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+  }
+  setTimeout(function(){alert("It's 10am!")}, millisTill10);

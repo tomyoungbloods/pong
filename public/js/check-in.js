e15fca73edc732ad0060fce877e4d8a875418a76
tomjongbloets <tomjongbloets@live.nl>
2019-11-28 12:16:21 +0100

    Inchecken scroll werkt nu

--- public/js/check-in.js
+++ public/js/check-in.js
@@ -12,6 +12,85 @@ function getAllPlayers() {
         success: function(response) {
             $('#players-loaded').empty().append(response);
             checkInPlayerClick();
+
+
+            
+            var myTable = jQuery('#myTable');
+            jQuery('body').keydown(function(){
+
+            var numberOfCells = jQuery('#myTable td').length;
+            var currentElement = jQuery('td.active');
+
+            if(event.keyCode == 37) {
+
+                var currentID = currentElement.attr('id').split('');
+                currentID.splice(currentID.length-1, 1, +currentID[currentID.length - 1]-1);
+
+                var newID = currentID.join('');
+                var newElement = jQuery('#' + newID);
+
+                if( newElement.length ){
+                    currentElement.removeClass('active');
+                    newElement.addClass('active');
+                } else {
+                    console.log('not found');
+                }
+
+            }
+
+            else if (event.keyCode == 39) {
+
+                var currentID = currentElement.attr('id').split('');
+                currentID.splice(currentID.length-1, 1, +currentID[currentID.length - 1]+1);
+
+                var newID = currentID.join('');
+                var newElement = jQuery('#' + newID);
+
+                if( newElement.length ){
+                    currentElement.removeClass('active');
+                    newElement.addClass('active');
+                } else {
+                    console.log('not found');
+                }
+
+
+            }
+               
+            else if (event.keyCode == 38) {
+
+                var currentID = currentElement.attr('id').split('');
+                currentID.splice(2,1, +currentID[2]-1);
+
+                var newID = currentID.join('');
+                var newElement = jQuery('#' + newID);
+
+                if( newElement.length ){
+                    currentElement.removeClass('active');
+                    newElement.addClass('active');
+                } else {
+                    console.log('not found');
+                }
+
+            }
+            else if (event.keyCode == 40) {
+
+                var currentID = currentElement.attr('id').split('');
+                currentID.splice(2,1, +currentID[2]+1);
+
+                var newID = currentID.join('');
+                var newElement = jQuery('#' + newID);
+
+                if( newElement.length ){
+                    currentElement.removeClass('active');
+                    newElement.addClass('active');
+                } else {
+                    console.log('not found');
+                }
+
+            }
+
+            });
+            
         },
         errors: function(response) {
             alert(response);
@@ -51,4 +130,4 @@ function checkInPlayerClick() {
 
 jQuery(document).ready(function($) {
     getAllPlayers();
-});
+}); 
--- resources/views/players/get-all.blade.php
+++ resources/views/players/get-all.blade.php
@@ -1,20 +1,65 @@
+@php
+    $row_counter = 1;
+@endphp
 <div class="row">
-    @foreach ($players as $player)
-        <div class="col-sm-2">
-            <div class="widget-holder">
-                <div class="checkin-plaats">
-                    <div class="checkin-naam">
-                        <a data-id="{{ $player->id }}" class="player-btn" href="#">
-                                <div class="checkin-image">
-                                        <img src="{{ $player['avatar_url'] }}">
+    <table id="myTable">                
+        @foreach ($players as $player)
+            @php
+                $open_row = false;
+                $close_row = false;
+                
+                if($loop->iteration % 6 == 1) {
+                    $open_row = true;
+                }
+                if($loop->iteration % 6 == 0) {
+                    $close_row = true;
+                }
+                $tab_index = 0;
+                if($loop->first) {
+                    $tab_index = "active";
+                }
+            @endphp
+            @if ($open_row)
+                <tr id="l0{{ $row_counter }}">                    
+                @php
+                    $row_counter++;
+                    $column_counter = 1;
+                @endphp
+            @endif
+                <td id="l0{{ $row_counter - 1 }}_{{ $column_counter }}" class="{{ $tab_index }}">
+                        <div class="checkin-plaats move" tabindex="0">
+                            <div class="checkin-naam">
+                                <a data-id="{{ $player->id }}" class="player-btn player-{{ $loop->iteration }}" href="#">
+                                        <div class="checkin-image">
+                                                <img src="{{ $player['avatar_url'] }}">
+                                        </div>
+                                    <div class="naambox @if($player->checked)checked-in @endif">
+                                    {{$player->name}}
+                                    </div>
+                                </a>
+                            </div> 
+                        </div>
+                </td>
+                @php($column_counter++)
+            @if ($close_row)
+                </tr>
+            @endif
+            {{-- <div class="col-sm-2">
+                <div class="widget-holder">
+                    <div class="checkin-plaats move" tabindex="0">
+                        <div class="checkin-naam">
+                            <a data-id="{{ $player->id }}" class="player-btn player-{{ $loop->iteration }}" href="#">
+                                    <div class="checkin-image">
+                                            <img src="{{ $player['avatar_url'] }}">
+                                    </div>
+                                <div class="naambox @if($player->checked)checked-in @endif">
+                                {{$player->name}}
                                 </div>
-                            <div class="naambox @if($player->checked)checked-in @endif">
-                            {{$player->name}}
-                            </div>
-                        </a>
-                    </div> 
+                            </a>
+                        </div> 
+                    </div>
                 </div>
-            </div>
-        </div>
-    @endforeach 
+            </div> --}}
+        @endforeach 
+    </table>
 </div>
